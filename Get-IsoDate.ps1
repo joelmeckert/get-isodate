831a42cd877bc06b76752c80234ede83db23487d
Joel Eckert <joel@joelmeckert.com>
2021-04-14 02:15:49 -0400

    Months were in the minutes field
    Late commit, found this was out of sync with a local version. Fixed an issue with the formatting, major, months were in the minutes field.

--- Get-IsoDate.ps1
+++ Get-IsoDate.ps1
@@ -3,25 +3,27 @@
 This code is licensed under MIT license (see LICENSE.txt for details)
 
 .SYNOPSIS
-	Gets the ISO 8601 datetime, it's unfortunate that it is such a complex process
+	Gets the ISO 8601 datetime, make it easy and readable on the console
 .DESCRIPTION
 	ISO 8601 datetime output, optionally output to a string that is viable for Windows / NTFS / FAT filesystems
 .PARAMETER Filename
 	Outputs the datetime object without the colon separator, for compatibility with Windows / NTFS / FAT filesystems
+.PARAMETER Console
+	Writes ISO 8601 datetime to console in a readable colour format, initially inspired by a bash shell script
 .INPUTS
 	None
 .OUTPUTS
-	ISO 8601 datetime, either as stdout, filename-compatible (without colons), or out to the console
+	ISO 8601 datetime, either as stdout, filename-compatible (without colons), or to console
 .NOTES
-	Version:        1.3.2
+	Version:        1.3.3
 	Author:         Joel Eckert
-	Creation Date:  2021-01-04
+	Creation Date:  2021-04-14
 	Purpose/Change: Consistent datetime tagging for logs and filenames 
 .EXAMPLE
 	Get-IsoDate
 	  Returns ISO 8601 datetime
 	Get-IsoDate -c
-	  Returns ISO 8601 datetime to console in a readable coloured format, initially inspired by a bash shell script
+	  Returns ISO 8601 datetime to console with Write-Host in a readable colour format
 	Get-IsoDate -f
 	  Returns pseudo-ISO 8601 datetime for a Windows filename
 #>
@@ -37,41 +39,34 @@ Function Get-IsoDate {
 		[switch]$Console
 	)
 	# Gets the current timezone and UTC offset
-	$TimeZone = (Get-TimeZone | Select-Object -ExpandProperty BaseUtcOffset).ToString()
-	$TimeZoneFormatted = $TimeZone.Substring(0, $TimeZone.Length - 3)
-		
+	$GetTimeZone = Get-TimeZone
+	$TimeZoneFormatted = '{0:d2}' -f ($GetTimeZone | Select-Object -ExpandProperty BaseUtcOffset | Select-Object -ExpandProperty Hours) + ":" + '{0:d2}' -f ($GetTimeZone | Select-Object -ExpandProperty BaseUtcOffset | Select-Object -ExpandProperty Minutes)
+	$DateTime = (Get-Date -Format yyyy-MM-ddTHH:mm:ss.fff) + $TimeZoneFormatted
+	
 	# Outputs the date in an ISO 8601-compliant fashion, with trailing zeros for the time zone (if not Newfoundland)
-	$DateTime = (Get-Date -Format yyyy-MM-ddTHH:MM:ss.fff) + $TimeZoneFormatted
 	If ($Filename.IsPresent) {
 		$DateTime = $DateTime -replace ":", "-"
+		Return $DateTime
 	}
 	ElseIf ($Console.IsPresent) {
-		$Year = $DateTime.Substring(0,4)
-		$Month = $DateTime.Substring(5,2)
-		$Day = $DateTime.Substring(8,2)
-		$Hour = $DateTime.Substring(11,2)
-		$Minute = $DateTime.Substring(14,2)
-		$Second = $DateTime.Substring(17,2)
-		$Nanosecond = $DateTime.Substring(20,3)
-		$TZ1 = $DateTime.Substring(23,3)
-		$TZ2 = $DateTime.Substring(27,3)
-
-		Write-Host $Year -ForegroundColor Green -NoNewLine
+		[regex]$rxDateTime = '^(?<Year>\d\d\d\d)-(?<Month>\d\d)-(?<Day>\d\d)T(?<Hour>\d\d):(?<Minute>\d\d):(?<Second>\d\d)\.(?<Millisecond>\d\d\d)(?<Offset1>(-|\+)\d\d):(?<Offset2>\d\d)$'
+		$Matches = $rxDateTime.Matches($DateTime)
+		Write-Host $Matches[0].Groups['Year'].Value -ForegroundColor Green -NoNewLine
 		Write-Host "-" -ForegroundColor Blue -NoNewLine
-		Write-Host $Month -ForegroundColor Green -NoNewLine
+		Write-Host $Matches[0].Groups['Month'].Value -ForegroundColor Green -NoNewLine
 		Write-Host "-" -ForegroundColor Blue -NoNewLine
-		Write-Host $Day -ForegroundColor Green -NoNewLine
+		Write-Host $Matches[0].Groups['Day'].Value -ForegroundColor Green -NoNewLine
 		Write-Host "T" -ForegroundColor Blue -NoNewLine
-		Write-Host $Hour -ForegroundColor Yellow -NoNewLine
+		Write-Host $Matches[0].Groups['Hour'].Value -ForegroundColor Yellow -NoNewLine
 		Write-Host ":" -ForegroundColor Blue -NoNewLine
-		Write-Host $Minute -ForegroundColor Yellow -NoNewLine
+		Write-Host $Matches[0].Groups['Minute'].Value -ForegroundColor Yellow -NoNewLine
 		Write-Host ":" -ForegroundColor Blue -NoNewLine
-		Write-Host $Second -ForegroundColor White -NoNewLine
+		Write-Host $Matches[0].Groups['Second'].Value -ForegroundColor White -NoNewLine
 		Write-Host "." -ForegroundColor Blue -NoNewLine
-		Write-Host $Nanosecond -ForegroundColor DarkGray -NoNewLine
-		Write-Host $TZ1 -ForegroundColor DarkGray -NoNewLine
+		Write-Host $Matches[0].Groups['Millisecond'].Value -ForegroundColor DarkGray -NoNewLine
+		Write-Host $Matches[0].Groups['Offset1'].Value -ForegroundColor DarkGray -NoNewLine
 		Write-Host ":" -ForegroundColor Blue -NoNewLine
-		Write-Host "$TZ2 " -ForegroundColor DarkGray -NoNewLine
+		Write-Host ($Matches[0].Groups['Offset2'].Value + " ") -ForegroundColor DarkGray -NoNewLine
 	}
 	Else {
 		Return $DateTime
